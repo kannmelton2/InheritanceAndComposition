2e3d66cd8b0885760973c6116744cea9017c42dc
Kayla Melton <kannmelton2@gmail.com>
2020-08-10 16:33:21 -0700

    Add project files.

---
+++ InheritanceAndComposition/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace InheritanceAndComposition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
